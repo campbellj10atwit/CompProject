a62e461496b0dbc04b31815250bef6362a43e935
Jonathan Campbell <campbellj10@wit.edu>
2021-04-20 14:25:27 -0400

    Update CompProject.java

--- src/edu/wit/cs/comp1050/CompProject/CompProject.java
+++ src/edu/wit/cs/comp1050/CompProject/CompProject.java
@@ -15,9 +15,8 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class CompProject extends Application{
-    int moves = 0;
     static Button arr[][] = new Button[3][3];
-    
+    //Static arr to be accessed by other methods
 
     public static void main(String[] args) {
         
@@ -26,28 +25,27 @@ public class CompProject extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        // TODO Auto-generated method stub
         
+        //declare the puzzle class and win boolean
         genpuz puz = new genpuz();
         boolean win = false;
         arr = puz.genpuz();
         int k = 1;
         Button blank = new Button();
-        
-        
+        //initializes blank button so it can be found in the array later
         Button rand = new Button("Randomize");
         rand.setStyle("-fx-font-size: 2em; ");
         BorderPane pane = new BorderPane();
         HBox bot = new HBox(rand);
         GridPane grid = new GridPane();
-        
+        //initializes new arrays until the puzzle is solvable
         while (!puz.isSolvable()) {
             arr = puz.genpuz();
             
             
         }
         
-        
+        //on the randomize button keypress, it initializes a new array and puts it into the gridpane
         rand.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent event) {
                 arr = puz.genpuz();
@@ -73,7 +71,6 @@ public class CompProject extends Application{
                 grid.add(arr[i][j], j, i);
             }
         }
-        Label move = new Label();
         
         
         grid.setVgap(10);
@@ -81,7 +78,7 @@ public class CompProject extends Application{
         pane.setCenter(grid);
         grid.setAlignment(Pos.CENTER);
         bot.setAlignment(Pos.BOTTOM_CENTER);
-        
+        //scene setup
         bot.setSpacing(15);
         move.setAlignment(Pos.TOP_CENTER);
         pane.setTop(move);
@@ -91,7 +88,7 @@ public class CompProject extends Application{
         stage.setTitle("Puzzle");
         stage.setScene(scene);
         stage.show();
-        
+        //On every mouse click, checks to see if the pane is in the correct order, if it is, the win screen appears and game is over
         scene.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -124,6 +121,7 @@ public class CompProject extends Application{
         
         
     }
+    //method to get a specific node from a specific point, for the win alg
     private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
         for (Node node : gridPane.getChildren()) {
             if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
